d84b9b4c790713d69a4a922b51fefa7f7e22e109
Nabil M. Al-Rousan <rousani@gmail.com>
2021-03-09 15:49:14 -0800

    insertion sort for linked list

--- Lab10/src/Sorting.java
+++ Lab10/src/Sorting.java
@@ -1,8 +1,62 @@
 import java.util.Arrays;
 
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+        next = null;
+    }
+}
+
 public class Sorting {
+
+
     // post-condition: this function print out the sorted array of the input
-    // this function should not alter the original array
+    // post-condition: this function should alter the orginal list
+    static void insertion_sort_for_linked_lists(Node head) {
+
+        Node new_list = null; //  5 -> 9
+
+        Node outer_iterator = head;
+        while(outer_iterator != null){//         // 5 -> 9 -> 7 -> 1 -> 2 ->null
+
+            if(new_list == null){
+                new_list = new Node(outer_iterator.data);
+            }else if (outer_iterator.data< new_list.data)
+            {
+                Node new_node = new Node(outer_iterator.data);
+                new_node.next = new_list;
+                new_list = new_node;
+            }
+            else {
+                Node i = new_list;
+                Node prev = i;
+                while (i.next != null && outer_iterator.data > i.data) {
+                    prev = i;
+                    i = i.next;
+                }
+                Node new_node = new Node(outer_iterator.data);
+                if(prev.next != null)
+                    new_node.next = i;
+
+                prev.next = new_node;
+            }
+
+
+            outer_iterator = outer_iterator.next;
+        }
+
+        Node iterator = new_list;
+        while(iterator != null){
+            System.out.print(iterator.data + " ");
+
+            iterator = iterator.next;
+        }
+
+    }
+    // post-condition: this function print out the sorted array of the input
+    // post-condition: this function should not alter the original array
     static void insertion_sort(int arr[]){
 
         for(int current_index_unsorted = 0 ; current_index_unsorted < arr.length; current_index_unsorted++){
@@ -23,7 +77,7 @@ public class Sorting {
     }
 
     // post-condition: this function print out the sorted array of the input
-    // this function should not alter the original array
+    // post-condition: this function should not alter the original array
     static void selection_sort( int arr[]){
             // 1- find the smallest element in the unsorted part of the array
         int smallest_index;
@@ -50,6 +104,15 @@ public class Sorting {
     public static void main (String arf[]){
         System.out.println("Hi");
         int [] arr = {23, 41, 33, 81, 07, 19, 11, 45};
-        insertion_sort(arr);
+//        insertion_sort(arr);
+
+
+        Node a = new Node(5);
+        a.next = new Node( 9);
+        a.next.next = new Node(7);
+        a.next.next.next = new Node(1);
+        a.next.next.next.next = new Node(2);
+        // 5 -> 9 -> 7 -> 1 -> 2 ->null
+        insertion_sort_for_linked_lists(a);
     }
 }
