eb46f571cb213669eba2fb8253c01c7a20c43cbd
Nabil M. Al-Rousan <rousani@gmail.com>
2021-03-16 13:26:53 -0700

    Lab 11 - union & intersection
    using dictionaries

--- Prelab11/src/Driver.java
+++ Prelab11/src/Driver.java
@@ -20,5 +20,55 @@ public class Driver {
         addressBook.put("Nabil",  new ExtendedAddress("nabil@plapla.com", "6044559872", "508 Thasd St. Coquitlam"));
         addressBook.put("Cody",   new ExtendedAddress("nabil@plapla.com", "6044559872", "508 Thasd St. Coquitlam"));
 
+        // New example: Union and Intersection of two arrays
+        Integer arr1 [] = {1, 2, 3, 4, 5};
+        Integer arr2 [] = {4, 5, 6, 7, 8};
+//        intersection_of_two_arrays(arr1, arr2); // should be 4, 5
+        union_of_two_arrays(arr1, arr2); // should be 1, 2, 3, 4, 5, 6, 7, 8,
+    }
+
+    private static void union_of_two_arrays(Integer[] arr1, Integer[] arr2) {
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int i = 0 ; i<arr1.length; i++){
+            if(frequencyMap.containsKey(arr1[i])){
+                frequencyMap.put(arr1[i],  frequencyMap.get(arr1[i]) + 1);
+            }else{
+                frequencyMap.put(arr1[i], 1);
+            }
+        }
+        for (int i = 0 ; i<arr2.length; i++){
+            if(frequencyMap.containsKey(arr2[i])){
+                frequencyMap.put(arr2[i],  frequencyMap.get(arr2[i]) + 1);
+            }else{
+                frequencyMap.put(arr2[i], 1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            System.out.println(entry.getKey());
+        }
+    }
+
+    private static void intersection_of_two_arrays(Integer[] arr1, Integer[] arr2) {
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int i = 0 ; i<arr1.length; i++){
+            if(frequencyMap.containsKey(arr1[i])){
+                frequencyMap.put(arr1[i],  frequencyMap.get(arr1[i]) + 1);
+            }else{
+                frequencyMap.put(arr1[i], 1);
+            }
+        }
+
+        for (int i = 0 ; i<arr2.length; i++){
+            if(frequencyMap.containsKey(arr2[i])){
+                frequencyMap.put(arr2[i],  frequencyMap.get(arr2[i]) + 1);
+            }else{
+                frequencyMap.put(arr2[i], 1);
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            if(entry.getValue() == 2)
+                System.out.println(entry.getKey());
+        }
     }
 }
