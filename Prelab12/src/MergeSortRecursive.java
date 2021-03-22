199767965ac380e3414503452aa946635bab2d90
Nabil <rousani@gmail.com>
2021-03-21 18:25:08 -0700

    Merge Sort - Iterative & efficient

--- Prelab12/src/MergeSortRecursive.java
+++ Prelab12/src/MergeSortRecursive.java
@@ -1,19 +1,45 @@
 public class MergeSortRecursive {
     static void mergesort(Integer [] arr,  Integer[] tmpArr, int firstIndex, int lastIndex){
 
-        if(firstIndex < lastIndex){
-            int middle = (firstIndex + lastIndex )/ 2;
-            mergesort(arr, tmpArr, firstIndex, middle);
-            mergesort(arr, tmpArr,middle + 1,lastIndex );
-            merge(arr,  tmpArr, firstIndex, middle, middle + 1, lastIndex);
-            System.arraycopy(tmpArr, firstIndex, arr, firstIndex, lastIndex - firstIndex + 1 );
+//        if(firstIndex < lastIndex){
+//            int middle = (firstIndex + lastIndex )/ 2;
+//            mergesort(arr, tmpArr, firstIndex, middle);
+//            mergesort(arr, tmpArr,middle + 1,lastIndex );
+//            merge(arr,  tmpArr, firstIndex, middle, middle + 1, lastIndex);
+//            System.arraycopy(tmpArr, firstIndex, arr, firstIndex, lastIndex - firstIndex + 1 );
+//        }
+
+        int length = 1;
+        while(length < arr.length){
+            int i = 0;
+            while(i <= lastIndex){
+                int L1 = i;
+                int R1 = L1 + length -1;
+                int L2= R1 + 1;
+                int R2 = L2 + length - 1;
+
+                if(L2 > lastIndex + 1)
+                    break;
+
+                // out of bound R2
+                if(R2 > lastIndex)
+                    R2 = lastIndex;
+
+                merge(arr,  tmpArr, L1, R1, L2 , R2);
+                System.arraycopy(tmpArr, L1, arr, L1, R2 - L1 + 1 );
+
+                i +=  (2 * length);
+             }
+            length *= 2;
         }
     }
+
     static void merge(Integer [] arr, Integer [] tmpArr,  int firstArrayFirstIndex, int firstArrayLastIndex,
                       int secondArrayFirstIndex, int secondArrayLastIndex){
 
         int index = firstArrayFirstIndex;
-        while(firstArrayFirstIndex <= firstArrayLastIndex && secondArrayFirstIndex <= secondArrayLastIndex){
+        while(firstArrayFirstIndex <= firstArrayLastIndex &&
+                secondArrayFirstIndex <= secondArrayLastIndex){
             // equality is for merging arrays of size 1
             if(arr[secondArrayFirstIndex] < arr[firstArrayFirstIndex]) {
                 tmpArr[index] = arr[secondArrayFirstIndex];
@@ -46,8 +72,6 @@ public class MergeSortRecursive {
         for (Integer x: tmpArr){
             System.out.print(x);
         }
-
-
 
         System.out.println();
 
